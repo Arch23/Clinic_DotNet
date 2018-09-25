ef0c5b40a6f07b1073f5bd0b52ae7c48a904e713
Arch23 <noda.94@gmail.com>
2018-09-25 10:25:12 -0300

    init commit

---
+++ Log/Creator.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Log
+{
+    public static class Creator
+    {
+        private static string FormatError(string pErrorMessage)
+        {
+            return $"[{DateTime.Now.ToString("dd/MM/yyyy-HH:mm:ss")}]: {pErrorMessage}";
+        }
+
+        public static void SaveLogBin(string pErrorMessage) {
+            LogData logData = new LogData {
+                Message = pErrorMessage,
+                Date = DateTime.Now
+            };
+
+            string FILE_NAME = "C://Logs//log.bin";
+            FileStream fs = null;
+
+            if (File.Exists(FILE_NAME)) {
+                fs = new FileStream(FILE_NAME, FileMode.Append);
+            }
+            else
+            {
+                fs = new FileStream(FILE_NAME, FileMode.Create);
+            }
+
+            BinaryFormatter bf = new BinaryFormatter();
+
+            bf.Serialize(fs, logData);
+
+            fs.Flush();
+            fs.Close();
+        }
+
+        public static LogData LoadLogBin()
+        {
+            LogData logData;
+
+            string FILE_NAME = "C://Logs//log.bin";
+
+            FileStream fs = new FileStream(FILE_NAME, FileMode.Open);
+
+            BinaryFormatter bf = new BinaryFormatter();
+
+            logData = (LogData)bf.Deserialize(fs);
+
+            fs.Close();
+
+            return logData;
+        }
+
+        public static void SaveLogXml(string pErrorMessage)
+        {
+            string FILE_NAME = "C://Logs//log.xml";
+            FileStream fs = new FileStream(FILE_NAME,FileMode.Create);
+
+            XmlSerializer xml = new XmlSerializer(typeof(LogData));
+
+            LogData logData = new LogData {
+                Message = pErrorMessage,
+                Date = DateTime.Now
+            };
+
+            xml.Serialize(fs, logData);
+
+            fs.Flush();
+            fs.Close();
+        }
+
+        public static LogData LoadLogXml()
+        {
+            string FILE_NAME = "C://Logs//log.xml";
+            FileStream fs = new FileStream(FILE_NAME, FileMode.Open);
+            XmlSerializer xml = new XmlSerializer(typeof(LogData));
+
+            LogData logData = (LogData)xml.Deserialize(fs);
+
+            fs.Close();
+
+            return logData;
+        }
+
+        public static void WriteLog(string pErrorMessage)
+        {
+            string FILE_NAME = "C://TSC_CLINIC//Logs//log.txt";
+            StreamWriter sw = null;
+
+            if (File.Exists(FILE_NAME))
+                sw = File.AppendText(FILE_NAME);
+            else
+                sw = new StreamWriter(FILE_NAME);
+
+            try
+            {
+                sw.WriteLine(FormatError(pErrorMessage));
+            }catch(Exception)
+            {
+                new StreamWriter($"{DateTime.Now.ToString("dd/MM/yyyy-HH:mm:ss")}.txt").WriteLine($"Log file blocked!\r\n{FormatError(pErrorMessage)}");
+            }
+            finally
+            {
+                if(sw != null)
+                {
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Log/LogData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log
+{
+    [Serializable]
+    public class LogData
+    {
+        private int SHIFT = 10;
+
+        public string Message { get { return Decode(Message); }
+            set {
+                Message = Encode(value);
+            }
+        }
+        public DateTime Date { get; set; }
+
+        private string Encode(string pMessage)
+        {
+            string result = "";
+
+            for(int i = 0; i < pMessage.Length; i++)
+            {
+                result += Convert.ToChar(((int)pMessage[i]) + SHIFT);
+            }
+
+            return (result);
+        }
+
+        private string Decode(string pMessage)
+        {
+            string result = "";
+
+            for (int i = 0; i < pMessage.Length; i++)
+            {
+                result += Convert.ToChar(((int)pMessage[i]) - SHIFT);
+            }
+
+            return (result);
+        }
+    }
+}
